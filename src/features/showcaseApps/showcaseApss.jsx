0513953c86637244dd41d94b8f21b2672f4f9ccc
duxante <spomenko.dukic@gmail.com>
2024-06-16 12:26:03 +0200

    creating new branch and showcaseApps component

---
+++ src/features/showcaseApps/showcaseApss.jsx
@@ -0,0 +1,25 @@
+import { ATHeadphones } from '../../assets';
+import MyButton from '../../common/button/myButton';
+import './showcaseApps.style.css';
+
+const ShowcaseApps = () => {
+  return (
+    <div className="showcaseAppsHolder">
+      <div className="showcaseAppsMain">
+        <div className="showcaseAppsLeft">
+          <h1>SHOWCASE APPS, INNOVATIVE TECH OR BUSINESSES.</h1>
+          <p>
+            Never be without the product you need! Sign Up today to save money
+            and time. Cancel anytime.
+          </p>
+          <MyButton customClass={'tryThisButton'} buttonText="ORDER NOW" />
+        </div>
+        <div className="showcaseAppsRight">
+          <ATHeadphones />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShowcaseApps;
